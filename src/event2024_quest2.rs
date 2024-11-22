a88f5e9618ec85d8d681a74e8d0abd67bff45d28
Daniel Flanagan <daniel@lyte.dev>
2024-11-22 08:47:43 -0600

    WIP part 2

--- src/event2024_quest2.rs
+++ src/event2024_quest2.rs
@@ -1,14 +1,28 @@
 use super::prelude::*;
 
+struct RunicNote<'a> {
+    needles: Vec<&'a str>,
+    haystack: &'a str,
+}
+
+impl<'a> RunicNote<'a> {
+    fn parse(input: &'a str) -> Self {
+        Self {
+            needles: (&input[6..input.find('\n').unwrap()]).split(",").collect(),
+            haystack: &input[input.find('\n').unwrap() + 2..],
+        }
+    }
+}
+
 pub struct Part1 {}
 impl QuestCompleter<i64> for Part1 {
     fn solve(input: &str) -> i64 {
-        let needles: Vec<&str> = (&input[6..input.find('\n').unwrap()]).split(",").collect();
-        let haystack: &str = &input[input.find('\n').unwrap() + 2..];
+        let RunicNote { needles, haystack } = RunicNote::parse(input);
         let mut result = 0;
         for n in 0..haystack.len() {
             for needle in &needles {
                 if needle.len() >= (haystack.len() - n) {
+                    // If the remainder of the haystack we're searching doesn't have enough characters, we skip on
                     continue;
                 }
                 if &haystack[n..n + needle.len()] == *needle {
@@ -38,7 +52,20 @@ mod part1test {
 pub struct Part2 {}
 impl QuestCompleter<i64> for Part2 {
     fn solve(input: &str) -> i64 {
-        666
+        let RunicNote { needles, haystack } = RunicNote::parse(input);
+        let mut result = 0;
+        for n in 0..haystack.len() {
+            for needle in &needles {
+                if needle.len() >= (haystack.len() - n) {
+                    // If the remainder of the haystack we're searching doesn't have enough characters, we skip on
+                    continue;
+                }
+                if &haystack[n..n + needle.len()] == *needle {
+                    result += needle.len() as i64
+                }
+            }
+        }
+        result
     }
 }
 
@@ -48,7 +75,18 @@ mod part2test {
 
     // #[test]
     fn example() {
-        assert_eq!(Part2::solve("part2example"), 4)
+        assert_eq!(
+            Part2::solve(
+                "WORDS:THE,OWE,MES,ROD,HER,QAQ
+
+AWAKEN THE POWE ADORNED WITH THE FLAMES BRIGHT IRE
+THE FLAME SHIELDED THE HEART OF THE KINGS
+POWE PO WER P OWE R
+THERE IS THE END
+QAQAQ"
+            ),
+            42
+        )
     }
 }
 
