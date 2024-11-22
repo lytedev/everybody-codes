c6fc34f61f298baae8ba3eb4dbc0a3bfea5357a4
Daniel Flanagan <daniel@lyte.dev>
2024-11-22 08:37:16 -0600

    Quest 2 Part 1 done

--- src/event2024_quest2.rs
+++ src/event2024_quest2.rs
@@ -3,7 +3,20 @@ use super::prelude::*;
 pub struct Part1 {}
 impl QuestCompleter<i64> for Part1 {
     fn solve(input: &str) -> i64 {
-        666
+        let needles: Vec<&str> = (&input[6..input.find('\n').unwrap()]).split(",").collect();
+        let haystack: &str = &input[input.find('\n').unwrap() + 2..];
+        let mut result = 0;
+        for n in 0..haystack.len() {
+            for needle in &needles {
+                if needle.len() >= (haystack.len() - n) {
+                    continue;
+                }
+                if &haystack[n..n + needle.len()] == *needle {
+                    result += 1
+                }
+            }
+        }
+        result
     }
 }
 
@@ -17,7 +30,7 @@ mod part1test {
             Part1::solve(
                 "WORDS:THE,OWE,MES,ROD,HER\n\nAWAKEN THE POWER ADORNED WITH THE FLAMES BRIGHT IRE"
             ),
-            777
+            4
         )
     }
 }
